8627e0b9171224de0b46f8ab6a08f14e262cc870
Samy Ali Sánchez <102321253+SamyAliSanchez@users.noreply.github.com>
2025-03-31 17:50:18 +0200

    feat: building and styling intro & header

---
+++ components/intro.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function Intro() {
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src="https://avatars.githubusercontent.com/u/102321253?v=4"
+              alt="Samy portrait"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl "
+            />
+          </motion.div>
+          <motion.span
+            className="absolute text-4xl bottom-0 right-0"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋🏽
+          </motion.span>
+        </div>
+      </div>
+    </section>
+  );
+}
