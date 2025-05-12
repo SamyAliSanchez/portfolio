eccfc874382752066ae84545d9c716b8ee84fc33
Samy Ali Sánchez <102321253+SamyAliSanchez@users.noreply.github.com>
2025-05-12 12:06:14 +0200

    feat: refactor

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import type { SectionName } from "./types";
 
+const AUTO_SCROLL_DELAY = 1000;
+
 export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
   const { ref, inView } = useInView({
     threshold,
@@ -10,7 +12,7 @@ export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
   const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
 
   useEffect(() => {
-    if (inView && Date.now() - timeOfLastClick > 1000) {
+    if (inView && Date.now() - timeOfLastClick > AUTO_SCROLL_DELAY) {
       setActiveSection(sectionName);
     }
   }, [inView, setActiveSection, timeOfLastClick, sectionName]);
